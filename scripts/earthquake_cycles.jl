b207f908c92919ceb7f381f9c9b84cec41e21f19
thehalfspace <thehalfspace@tutanota.com>
2019-06-23 14:03:07 -0400

    remove deprecated syntax from scripts

--- scripts/earthquake_cycles.jl
+++ scripts/earthquake_cycles.jl
@@ -17,20 +17,20 @@ function plotHypo(hypo)  #S, Slip, SlipVel, Stress, time_)
     hist = fit(Histogram, -hypo./1e3, closed=:right, nbins=10)
 
     fig = PyPlot.figure(figsize=(5,7))
-    ax = fig[:add_subplot](111)
+    ax = fig.add_subplot(111)
 
-    ax[:barh](hist.edges[1][1:end-1], hist.weights)
+    ax.barh(hist.edges[1][1:end-1], hist.weights)
     #  ax[:plot](collect(1:80), -8*ones(80), "k--", label="Fault Zone Depth")
-    ax[:set_xlabel]("Number of Earthquakes")
-    ax[:set_ylabel]("Depth (km)")
-    ax[:set_title]("Hypocenter Location")
-    ax[:set_ylim]([0, 24])
+    ax.set_xlabel("Number of Earthquakes")
+    ax.set_ylabel("Depth (km)")
+    ax.set_title("Hypocenter Location")
+    ax.set_ylim([0, 24])
     ax.invert_yaxis()
     fig.tight_layout()
     show()
 
     figname = string(path, "hypo.png")
-    fig[:savefig](figname, dpi = 300)
+    fig.savefig(figname, dpi = 300)
 
 end
 
@@ -193,19 +193,19 @@ end
 function scaling(a,b)
 
     fig = PyPlot.figure(figsize=(12,9))
-    ax = fig[:add_subplot](111)
+    ax = fig.add_subplot(111)
 
-    ax[:plot](a,b, "k.", markersize=20)
-    ax[:set_xlabel]("Slip (m)")
-    ax[:set_ylabel]("Rupture Length (m)")
-    #  ax[:set_yscale]("log")
-    #  ax[:set_xscale]("log")
-    ax[:set_title]("Rupture length vs Slip")
-    #  ax[:legend](loc="upper right")
+    ax.plot(a,b, "k.", markersize=20)
+    ax.set_xlabel("Slip (m)")
+    ax.set_ylabel("Rupture Length (m)")
+    #  ax.set_yscale("log")
+    #  ax.set_xscale("log")
+    ax.set_title("Rupture length vs Slip")
+    #  ax.legend(loc="upper right")
     show()
 
     figname = string(path, "scaling1.png")
-    fig[:savefig](figname, dpi = 300)
+    fig.savefig(figname, dpi = 300)
 end
 
 
@@ -220,21 +220,21 @@ function MwPlot(Mw)
     cum = cumsum(hist.weights[end:-1:1])[end:-1:1]
 
     fig = PyPlot.figure(figsize=(8,7))
-    ax = fig[:add_subplot](111)
+    ax = fig.add_subplot(111)
 
-    #  ax[:plot](hist.edges[1][1:end-1], hist.weights, ".", label="Non-cumulative")
-    ax[:plot](hist.edges[1][1:end-1], cum, "k.--", markersize=20) #, label="Cumulative")
-    #  ax[:set_xlabel]("Moment Magnitude (Mw)")
-    #  ax[:set_ylabel]("Number of Earthquakes")
-    ax[:set_yscale]("log")
-    #  ax[:set_title]("Magnitude-frequency distribution")
-    #  ax[:set_xlim]([2, 7])
-    ax[:set_ylim]([1, 30])
-    #  ax[:legend](loc="upper right")
+    #  ax.plot](hist.edges[1][1:end-1], hist.weights, ".", label="Non-cumulative")
+    ax.plot(hist.edges[1][1:end-1], cum, "k.--", markersize=20) #, label="Cumulative")
+    #  ax.set_xlabel("Moment Magnitude (Mw)")
+    #  ax.set_ylabel("Number of Earthquakes")
+    ax.set_yscale("log")
+    #  ax.set_title("Magnitude-frequency distribution")
+    #  ax.set_xlim([2, 7])
+    ax.set_ylim([1, 30])
+    #  ax.legend(loc="upper right")
     show()
 
     figname = string(path, "mfd.png")
-    fig[:savefig](figname, dpi = 300)
+    fig.savefig(figname, dpi = 300)
 end
 
 function MwPlot2(Mw1, Mw2, Mw3, Mw4, Mw5, Mw6)
@@ -254,24 +254,24 @@ function MwPlot2(Mw1, Mw2, Mw3, Mw4, Mw5, Mw6)
     cum5 = cumsum(hist5.weights[end:-1:1])[end:-1:1]
 
     fig = PyPlot.figure(figsize=(12,9))
-    ax = fig[:add_subplot](111)
+    ax = fig.add_subplot(111)
 
-    ax[:plot](hist1.edges[1][1:end-1], cum1, ".-", markersize=10, label="400 m")
-    ax[:plot](hist2.edges[1][1:end-1], cum2, ".-", markersize=10, label="320 m")
-    ax[:plot](hist3.edges[1][1:end-1], cum3, ".-", markersize=10, label="250 m")
-    ax[:plot](hist4.edges[1][1:end-1], cum4, ".-", markersize=10, label="160 m")
-    ax[:plot](hist5.edges[1][1:end-1], cum5, ".-", markersize=10, label="100 m")
-    ax[:set_xlabel]("Moment Magnitude (Mw)")
-    ax[:set_ylabel]("Number of Earthquakes")
-    ax[:set_yscale]("log")
-    ax[:set_title]("Magnitude-frequency distribution")
-    #  ax[:set_xlim]([1.7, 7])
-    ax[:set_ylim]([1e-1, 1e3])
-    ax[:legend](loc="upper right")
+    ax.plot(hist1.edges[1][1:end-1], cum1, ".-", markersize=10, label="400 m")
+    ax.plot(hist2.edges[1][1:end-1], cum2, ".-", markersize=10, label="320 m")
+    ax.plot(hist3.edges[1][1:end-1], cum3, ".-", markersize=10, label="250 m")
+    ax.plot(hist4.edges[1][1:end-1], cum4, ".-", markersize=10, label="160 m")
+    ax.plot(hist5.edges[1][1:end-1], cum5, ".-", markersize=10, label="100 m")
+    ax.set_xlabel("Moment Magnitude (Mw)")
+    ax.set_ylabel("Number of Earthquakes")
+    ax.set_yscale("log")
+    ax.set_title("Magnitude-frequency distribution")
+    #  ax.set_xlim([1.7, 7])
+    ax.set_ylim([1e-1, 1e3])
+    ax.legend(loc="upper right")
     show()
 
     figname = string(path, "mfdcomp.png")
-    fig[:savefig](figname, dpi = 300)
+    fig.savefig(figname, dpi = 300)
 end
 
 #.................................
@@ -281,14 +281,14 @@ end
 function eq_catalog(Mw, t_catalog, yr2sec)
 
     fig = PyPlot.figure(figsize=(12,9))
-    ax = fig[:add_subplot](111)
+    ax = fig.add_subplot(111)
 
-    ax[:scatter](t_catalog./yr2sec, Mw, s= 30, marker=".")
-    ax[:set_xlabel]("Time (yrs)")
-    ax[:set_ylabel]("Moment Magnitude (Mw)")
-    ax[:set_title]("Earthquake Catalogue")
+    ax.scatter(t_catalog./yr2sec, Mw, s= 30, marker=".")
+    ax.set_xlabel("Time (yrs)")
+    ax.set_ylabel("Moment Magnitude (Mw)")
+    ax.set_title("Earthquake Catalogue")
     show()
 
     figname = string(path, "catalogue.pdf")
-    fig[:savefig](figname, dpi = 300)
+    fig.savefig(figname, dpi = 300)
 end
